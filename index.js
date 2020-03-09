b56be9e82839be7cbe5511b53572d16496555f78
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 19:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 let totalBatteries = 0;
 
 let reducer = batteryBatches.reduce(function(totalBatteries, element){
-  return totaBatteries + element}
+  return totalBatteries + element}
 )
 
 reducer
